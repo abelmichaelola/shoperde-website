11aae9836defde89ed1f0476e83210d080cceec7
abelmichaelola <abelmichaelola@gmail.com>
2021-05-24 22:38:10 +0800

    first commit

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Redirect, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import styles from "./HomePage.module.scss";
 import logo from "./../../assets/logo.png";
 import CategoryList from "../LeftView/CategoryList/CategoryList";
